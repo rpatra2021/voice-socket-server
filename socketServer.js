db4c1f4f06c211f4f81f6c72818ffb2f64698029
Rahul Patra <official.rpatra@gmail.com>
2023-06-05 11:42:31 +0530

    send message

--- socketServer.js
+++ socketServer.js
@@ -1,3 +1,37 @@
-const server=require("http").createServer(),io=require("socket.io")(server),SOCKET_PORT=process.env.SOCKET_PORT||8080;server.listen(SOCKET_PORT,function(){console.log("Server listening socket at port",SOCKET_PORT)});
+const server=require("http").createServer();
+const io=require("socket.io")(server);
+const SOCKET_PORT=process.env.SOCKET_PORT||8080;
 
-io.on("connection",function(o){o.on("room join",e=>{o.room=e.room,o.join(e.room),o.join(e.socketId),o.to(o.room).emit("new joined",{socketId:e.socketId,userId:e.userId,userName:e.userName})}),o.on("old user",e=>{o.to(e.to).emit("old user",{sender:e.sender,userId:e.userId,userName:e.userName})}),o.on("pwc",e=>{o.to(e.to).emit("pwc",{description:e.description,sender:e.sender})}),o.on("ice candidates",e=>{o.to(e.to).emit("ice candidates",{candidate:e.candidate,sender:e.sender})}),o.on("leave call",e=>{n()}),o.on("disconnect",function(){n()});const n=function(){o.to(o.room).emit("leave call",{socketId:o.id}),o.disconnect(!0)}});
+server.listen(SOCKET_PORT,function(){
+    console.log("Server listening socket at port",SOCKET_PORT)
+});
+
+io.on("connection",function(o){
+    o.on("room join",e=>{
+        o.room=e.room,
+        o.join(e.room),
+        // o.join(e.socketId),
+        o.to(o.room).emit("new joined",{socketId:e.socketId,userId:e.userId,userName:e.userName})
+    }),
+    o.on("old user",e=>{
+        o.to(e.to).emit("old user",{sender:e.sender,userId:e.userId,userName:e.userName})
+    }),
+    o.on("pwc",e=>{
+        o.to(e.to).emit("pwc",{description:e.description,sender:e.sender})
+    }),
+    o.on("ice candidates",e=>{
+        o.to(e.to).emit("ice candidates",{candidate:e.candidate,sender:e.sender})
+    }),
+    o.on("new message",e=>{
+        o.to(o.room).emit("new message",e)
+    }),
+    o.on("leave call",e=>{
+        n()
+    }),
+    o.on("disconnect",function(){
+        n()
+    });
+    const n=function(){
+        o.to(o.room).emit("leave call",{socketId:o.id}),o.disconnect(!0)
+    }
+});
